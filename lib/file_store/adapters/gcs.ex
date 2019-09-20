a2a115c0c89d83daa3d6c97f9043acc83b39cdc3
Ray Zane <raymondzane@gmail.com>
2019-09-20 13:43:12 -0400

    Install dependencies

--- lib/file_store/adapters/gcs.ex
+++ lib/file_store/adapters/gcs.ex
@@ -1,18 +1,3 @@
 defmodule FileStore.Adapters.GCS do
-  @moduledoc """
-  Documentation for FileStore.Adapters.GCS.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FileStore.Adapters.GCS.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
