2b31087e22bb03df0cacdb140db4ccc5a30cf4bc
Ray Zane <raymondzane@gmail.com>
2019-09-21 12:15:42 -0400

    Keep track of the upload in a struct

--- lib/file_store/adapters/gcs/upload.ex
+++ lib/file_store/adapters/gcs/upload.ex
@@ -1,47 +1,39 @@
 defmodule FileStore.Adapters.GCS.Upload do
+  @moduledoc false
+
   alias FileStore.Adapters.GCS.Client
 
-  defstruct [
-    :id,
-    :key,
-    :path,
-    :bucket,
-    :client,
-    total_bytes: 0,
-    sent_bytes: 0
-  ]
+  defstruct [:id, :bucket, :client, size: 0, start_byte: 0]
 
-  @header "X-GUploader-UploadID"
+  @header "x-guploader-uploadid"
   @chunk_size 5 * 1024 * 1024
 
   def perform(client, bucket, path, key) do
-    upload = %__MODULE__{key: key, path: path, bucket: bucket, client: client}
-
-    with {:ok, %{size: total_bytes}} <- File.stat(path),
+    with {:ok, %{size: size}} <- File.stat(path),
          {:ok, %{headers: headers}} <- Client.start_upload(client, bucket, key),
          {:ok, id} <- fetch_header(headers, @header) do
-      stream_file(%Upload{upload | id: id, total_bytes: total_bytes})
+      upload = %__MODULE__{id: id, size: size, bucket: bucket, client: client}
+
+      path
+      |> File.stream!([], @chunk_size)
+      |> reduce_while_ok(upload, &send_chunk(&2, &1))
     end
   end
 
-  defp stream_file(%__MODULE__{path: path} = upload) do
-    path
-    |> File.stream!([], @chunk_size)
-    |> reduce_while_ok(upload, &send_chunk/2)
+  defp send_chunk(%__MODULE__{id: id, bucket: bucket, client: client} = upload, chunk) do
+    headers = build_headers(upload, chunk)
+
+    with {:ok, resp} <- Client.upload_part(client, bucket, id, chunk, headers),
+         {:ok, end_byte} <- fetch_range_end(resp.headers) do
+      {:ok, %__MODULE__{upload | start_byte: end_byte + 1}}
+    end
   end
 
-  defp send_chunk(chunk, %__MODULE__{id: id, bucket: bucket, sent_bytes: sent_bytes}) do
+  defp build_headers(%__MODULE__{size: size, start_byte: start_byte}, chunk) do
     chunk_size = byte_size(chunk)
-
-    headers = [
-      {"Content-Length", chunk_size},
-      {"Content-Range", "bytes #{sent_bytes}-#{sent_bytes + chunk_size - 1}/#{size}"}
-    ]
-
-    with {:ok, resp} <- Client.upload_part(client, bucket, upload_id, chunk, headers) do
-      # Use `Range` response header.
-      {:ok, %__MODULE__{upload | sent_bytes: sent_bytes + chunk_size}}
-    end
+    end_byte = start_byte + chunk_size - 1
+    range = "bytes #{start_byte}-#{end_byte}/#{size}"
+    [{"Content-Length", chunk_size}, {"Content-Range", range}]
   end
 
   defp reduce_while_ok(enumerable, initial, fun) do
@@ -51,6 +43,18 @@ defmodule FileStore.Adapters.GCS.Upload do
         {:error, error} -> {:halt, {:error, error}}
       end
     end)
+  end
+
+  defp fetch_range_end(headers) do
+    with {:ok, range} <- fetch_header(headers, "range") do
+      {end_byte, _} =
+        range
+        |> String.split("-")
+        |> Enum.at(1)
+        |> Integer.parse()
+
+      {:ok, end_byte}
+    end
   end
 
   defp fetch_header(headers, name) do
